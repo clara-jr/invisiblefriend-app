1b8cf629fbf6209a1fdb88bb22dea6c4fb75a09a
Clara Jimenez <clarajimenezrecio@gmail.com>
2017-11-25 23:04:31 +0100

    Amigo Invisible: No dejar registrarse en un grupo ya cerrado

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -21,34 +21,48 @@ exports.signup = function(req, res) {
             password: req.body.password,
             groupId: req.body.group
         });
-        Model.findOne({
-            name: req.body.login,
+        Model.find({
             groupId: req.body.group
-        }, function(err, existent) {
-            if(err) {
-                req.session.errors = {"message": 'Se ha producido un error: '+err};
-                console.log(req.session.errors.message);
-                res.redirect('/user');
-                return;
-            }
-            if (existent) {
-                req.session.errors = {"message": 'Este usuario ya existe'};
+        }, function(err, model) {
+            if(err) res.send(500, err.message);
+            console.log('GET /participants');
+            if (model.length>0 && model[0].visible) {
+                req.session.errors = {"message": 'Este grupo ya ha cerrado el sorteo'};
                 console.log(req.session.errors);
                 console.log(req.session.errors.message);
                 res.redirect('/user');
                 return;
             } else {
-                participant.save(function(err, participant) {
+                Model.findOne({
+                    name: req.body.login,
+                    groupId: req.body.group
+                }, function(err, existent) {
                     if(err) {
                         req.session.errors = {"message": 'Se ha producido un error: '+err};
-                        res.redirect("/user");        
+                        console.log(req.session.errors.message);
+                        res.redirect('/user');
                         return;
                     }
-                    req.session.user = {id:participant.id, username:participant.name, group:participant.groupId};
-                    console.log(req.session.user);
-                    console.log(req.session.user.id);
-                    console.log(req.session.user.username);
-                    res.redirect('/participants');
+                    if (existent) {
+                        req.session.errors = {"message": 'Este usuario ya existe'};
+                        console.log(req.session.errors);
+                        console.log(req.session.errors.message);
+                        res.redirect('/user');
+                        return;
+                    } else {
+                        participant.save(function(err, participant) {
+                            if(err) {
+                                req.session.errors = {"message": 'Se ha producido un error: '+err};
+                                res.redirect("/user");        
+                                return;
+                            }
+                            req.session.user = {id:participant.id, username:participant.name, group:participant.groupId};
+                            console.log(req.session.user);
+                            console.log(req.session.user.id);
+                            console.log(req.session.user.username);
+                            res.redirect('/participants');
+                        });
+                    }
                 });
             }
         });
